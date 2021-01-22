c24a1d675cd00bce871a8d1c83b39cd3747bb4e6
Cesar Pablo Anco Jove <cesa_anco_jove@hotmail.com>
2021-01-22 16:31:45 -0500

    se agrego mas

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,13 +4,20 @@
 
 
 <h1>Dimencionamiento de lineas</h1>
-
-<label for="">Voltaje de la fuente</label>
-<input type="text">
-<label for="">Resistitividad de la linea</label>
-<input type="text">
-
+<div class="entrada">
+    <label for="">Voltaje de la fuente</label>
+    <input type="text">
+    <label for="">Resistitividad de la linea</label>
+    <input type="text">
+    <label for="">Longitud de la linea</label>
+    <input type="text">
+    <label for="">Resistencia de carga</label>
+    <input type="text">
+</div>
 
 <style>
-
+    .entrada{
+        display: flex;
+        flex-direction: column;
+    }
 </style>
