05d854cdc30080f221bb8502a7b701e14403a62d
Cesar Pablo Anco Jove <cesa_anco_jove@hotmail.com>
2021-01-22 14:58:10 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+
+<h1>Dimencionamiento de lineas</h1>
+
+<label for="">Voltaje de la fuente</label>
+<input type="text">
+<label for="">Resistitividad de la linea</label>
+<input type="text">
+
+
+<style>
+
+</style>
