3a2a373cab95a50c69fbe0239f78ef220ff6f0f5
Cesar P. Anco Jove <cesar_anco_jove@hotmail.com>
2021-01-29 23:15:40 -0500

    Se agrego Numerro de baterias

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,51 @@
+
+<script>
+	let ener = ""
+	let v_inv = ""
+	let v_bateria = ""
+	let capasidad = ""
+	let dop = ""
+
+	$: b_serie = parseFloat(v_inv)/parseFloat(v_bateria)
+	$: cap_total = parseFloat(ener)*1000/parseFloat(v_inv)
+	$: b_paralelo = Math.ceil(cap_total / (parseFloat(capasidad) * parseFloat(dop)/100))
+
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>Numero de baterias</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<div class="entrada">
+	<label for="">Energia que se proveera a la carga (KW-H)</label>
+	<input type="text" bind:value={ener}>
+	
+	<label for="">Voltaje DC de entrada del banco de baterias al inversor</label>
+	<input type="text" bind:value={v_inv}>
+	
+	<label for="">Voltaje de la bateria a utilizar</label>
+	<input type="text" bind:value={v_bateria}>
+	
+	<label for="">Capacidad de la bateria a utilizar</label>
+	<input type="text" bind:value={capasidad}>
+	
+	<label for="">Profundidad de descarga de la bateria (%)</label>
+	<input type="text" bind:value={dop}>
 
+	<label for="">Baterias en serie: {b_serie}</label>
+	<label for="">Cantidad de paralelos: {b_paralelo}</label>
+	<label for="">Numero de Baterias en total: {b_serie*b_paralelo}</label>
+
+</div>
+
+
+
+<style>
+    .entrada{
+        display: flex;
+        flex-direction: column;
+    }
+
+</style>
